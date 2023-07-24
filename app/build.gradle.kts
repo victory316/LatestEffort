632b801c4b5b5fca3eedc283e3cc3f97e6ecb517
SH CHoi <csh153@gmail.com>
2023-07-24 17:00:26 +0900

    신규 릴리즈 준비

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.choidev.latesteffort"
         minSdk = 28
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
