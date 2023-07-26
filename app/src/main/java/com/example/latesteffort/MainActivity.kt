110774d496cc2857abb298614475eae704a91d5d
SH CHoi <csh153@gmail.com>
2023-07-26 17:32:21 +0900

    액티비티 확장함수 추가

--- app/src/main/java/com/example/latesteffort/MainActivity.kt
+++ app/src/main/java/com/example/latesteffort/MainActivity.kt
@@ -2,21 +2,18 @@ package com.example.latesteffort
 
 import LeTheme
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.choidev.core.actions.Action
 import com.choidev.core.actions.NavigateAction
-import com.choidev.core.actions.NavigateAction.NavGraphDestination
-import com.choidev.core.actions.NavigateAction.StartActivity
 import com.choidev.core.actions.SystemAction
 import com.choidev.core.actions.VibrateAction
-import com.choidev.core.actions.mapToId
 import com.choidev.core.actions.presenter.SimpleActionPresenter
 import com.choidev.latesteffort.core.util.vibration.VibrationManager
-import com.example.latesteffort.ext.startNewActivity
+import com.example.latesteffort.ext.handleNavigateAction
+import com.example.latesteffort.ext.handleSystemAction
+import com.example.latesteffort.ext.handleVibrationAction
 import com.example.latesteffort.navigation.LeNavHost
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -57,58 +54,6 @@ class MainActivity : ComponentActivity() {
                     navController = navController,
                     presenter = presenter
                 )
-            }
-        }
-    }
-
-    private fun handleNavigateAction(
-        navController: NavHostController,
-        action: NavigateAction
-    ) {
-        when (action) {
-            is NavGraphDestination -> {
-                navController.navigate(action.destination)
-            }
-
-            is StartActivity -> {
-                startNewActivity(action.screenClass)
-            }
-        }
-    }
-
-    private fun handleSystemAction(action: SystemAction) {
-        when (action) {
-            is SystemAction.ShowToast -> {
-                Toast.makeText(this, action.message, Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    private fun handleVibrationAction(action: VibrateAction) {
-        when (action) {
-            is VibrateAction.Vibrate -> {
-                if (action.activate) {
-                    vibrationManager.vibrate(
-                        duration = action.duration,
-                        amplitude = action.amplitude
-                    )
-                }
-            }
-
-            is VibrateAction.VibrateEffect -> {
-                vibrationManager.vibrateEffect(action.effect.mapToId())
-            }
-
-            is VibrateAction.VibratePattern -> {
-                vibrationManager.vibrateWithPattern(
-                    timing = action.patterns.map { it.first.toLong() }.toLongArray(),
-                    amplitudes = action.patterns.map { it.second }.toIntArray(),
-                    repeat = if (action.repeat) 0 else -1
-                )
-            }
-
-            VibrateAction.StopVibration -> {
-                vibrationManager.stopVibration()
             }
         }
     }
