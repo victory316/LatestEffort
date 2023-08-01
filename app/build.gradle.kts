6223d585280d59ddcd93e62a2e5e60b29d64939f
SH CHoi <csh153@gmail.com>
2023-08-01 14:30:13 +0900

    버전 코드 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.choidev.latesteffort"
         minSdk = 28
         targetSdk = 33
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
