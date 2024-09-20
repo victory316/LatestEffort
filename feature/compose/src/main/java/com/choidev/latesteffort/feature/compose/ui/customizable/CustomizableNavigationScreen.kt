33c433b6ff26f673d9f92cb15b4196609559c8b7
SH Choi <csh153@gmail.com>
2024-09-20 14:53:05 +0900

    Flow 구성중

--- feature/compose/src/main/java/com/choidev/latesteffort/feature/compose/ui/customizable/CustomizableNavigationScreen.kt
+++ feature/compose/src/main/java/com/choidev/latesteffort/feature/compose/ui/customizable/CustomizableNavigationScreen.kt
@@ -3,8 +3,11 @@ package com.choidev.latesteffort.feature.compose.ui.customizable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.choidev.latesteffort.feature.compose.ui.customizable.state.BoxInfoState
 
 @Composable
@@ -12,6 +15,8 @@ fun CustomizableNavigationScreen(
     modifier: Modifier = Modifier,
     viewModel: CustomizableViewModel = hiltViewModel()
 ) {
+    val screenState by viewModel.screenState.collectAsStateWithLifecycle()
+
     Box(modifier = modifier.fillMaxSize()) {
     }
 }
--- feature/compose/src/main/java/com/choidev/latesteffort/feature/compose/ui/customizable/CustomizableViewModel.kt
+++ feature/compose/src/main/java/com/choidev/latesteffort/feature/compose/ui/customizable/CustomizableViewModel.kt
@@ -1,10 +1,30 @@
 package com.choidev.latesteffort.feature.compose.ui.customizable
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.choidev.latesteffort.feature.compose.ui.customizable.state.BoxInfoState
+import com.choidev.latesteffort.feature.compose.ui.customizable.state.CustomizableScreenState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class CustomizableViewModel @Inject constructor() : ViewModel() {
 
+    private val _screenState = MutableStateFlow(CustomizableScreenState())
+    val screenState =
+        _screenState.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = CustomizableScreenState()
+        )
+
+    init {
+        _screenState.update {
+            it.copy(boxes = listOf(BoxInfoState(2, 3)))
+        }
+    }
 }
