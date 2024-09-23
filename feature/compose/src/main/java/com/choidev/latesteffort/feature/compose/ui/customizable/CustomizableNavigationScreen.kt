521f2e4472bd4eeb370ab954df4c56711aea3bdd
SH Choi <csh153@gmail.com>
2024-09-23 15:42:44 +0900

    Sticky box 테스트

--- feature/compose/src/main/java/com/choidev/latesteffort/feature/compose/ui/customizable/CustomizableNavigationScreen.kt
+++ feature/compose/src/main/java/com/choidev/latesteffort/feature/compose/ui/customizable/CustomizableNavigationScreen.kt
@@ -1,9 +1,6 @@
 package com.choidev.latesteffort.feature.compose.ui.customizable
 
-import android.util.Log
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,23 +15,21 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.choidev.latesteffort.feature.compose.ui.customizable.state.BoxInfoState
+import kotlin.math.absoluteValue
 
 @Composable
 fun CustomizableNavigationScreen(
@@ -56,59 +51,51 @@ fun CustomizableNavigationScreen(
                 .padding(20.dp)
         ) {
             screenState.boxes.forEach { box ->
-                ResizableBox(state = box)
+                PositionStickyBox(state = box)
             }
         }
     }
 }
 
 @Composable
-fun ResizableBox(state: BoxInfoState) {
+fun PositionStickyBox(state: BoxInfoState) {
+    val pxValue = with(LocalDensity.current) { 20.dp.toPx() }
     val rowSize = 50.dp * state.sizeRow
     var columnSize = 50.dp * state.sizeColumn
-    var rowPosSize by remember { mutableStateOf(20.dp * state.posRow) }
-    var columnPosSize by remember { mutableStateOf(20.dp * state.posColumn) }
-    var boxZoom by remember { mutableStateOf(0f) }
-
-    var offset by remember { mutableStateOf(Offset.Zero) }
+    var offsetX by remember { mutableFloatStateOf(pxValue * state.posRow) }
+    var offsetY by remember { mutableFloatStateOf(pxValue * state.posColumn) }
+    var boxZoom by remember { mutableFloatStateOf(0f) }
 
     Box(
         modifier = Modifier
             .width(rowSize)
             .height(columnSize)
-            .offset(x = rowPosSize, y =  columnPosSize)
             .pointerInput(Unit) {
-                detectTransformGestures { centroid, pan, zoom, rotation ->
-                    boxZoom = maxOf(1f, boxZoom * zoom)
-                    rowPosSize = centroid.x.dp
-                    columnPosSize = centroid.y.dp
+                detectDragGestures { change, dragAmount ->
+                    change.position.x.run {
+                        if ((this - offsetX).absoluteValue.toDp() >= 50.dp) {
+                            offsetX = this
+                        }
+                    }
+
+                    change.position.y.run {
+                        if ((this - offsetY).absoluteValue.toDp() >= 50.dp) {
+                            offsetY = this
+                        }
+                    }
                 }
+            }
+            .offset {
+                IntOffset(x = offsetX.toInt(), y = offsetY.toInt())
+            }
+            .drawBehind {
+                drawRect(Color.White)
             }
             .graphicsLayer {
                 scaleX = boxZoom
                 scaleY = boxZoom
             }
-            .drawBehind {
-                drawRect(color = Color.Green)
-            }
     ) {
         Text(text = "Hello! I'm a new box")
     }
 }
-
-fun Offset.calculateNewOffset(
-    centroid: Offset,
-    pan: Offset,
-    zoom: Float,
-    gestureZoom: Float,
-    size: IntSize
-): Offset {
-    val newScale = maxOf(1f, zoom * gestureZoom)
-    val newOffset = (this + centroid / zoom) -
-            (centroid / newScale + pan / zoom)
-    return Offset(
-        newOffset.x.coerceIn(0f, (size.width / zoom) * (zoom - 1f)),
-        newOffset.y.coerceIn(0f, (size.height / zoom) * (zoom - 1f))
-    )
-}
-
